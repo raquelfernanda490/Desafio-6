95ee22f3a5015e7f31f725e7cc3d662b6b47eacb
user.raquelfernanda490 <user.raquelfernanda490@gmail.com>
2023-09-28 15:14:14 -0300

    atualização de código

--- script.js
+++
@@ -1,8 +0,0 @@
-var n1 = parseFloat(prompt('Digite um número:'));
-
-if(n1 > 0 && n1 < 100 && n1 % 2 === 0){
-    alert('O número atende aos critérios')
-}else{
-    alert('O número NÃO atende aos critérios');
-}
-
